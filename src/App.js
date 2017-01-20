b8720902dc15fdeb1a5d029922c28f71695b45c7
Josh Humpherys <joshua.humpherys@gmail.com>
2017-01-20 16:45:50 -0500

    Add Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, OverlayTrigger, Popover } from 'react-bootstrap';
 
 class App extends Component {
   render() {
+    const popover = (
+      <Popover
+        id="popover-positioned-top"
+        title="Popover top"
+      >
+        And here's some <strong>amazing</strong> content. It's very engaging. right?
+      </Popover>
+    );
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +22,12 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button bsStyle="primary">hello world</Button>
+        <div style={{ height: 120 }}>
+        </div>
+        <OverlayTrigger trigger="click" placement="top" overlay={popover}>
+          <Button>Holy guacamole!</Button>
+        </OverlayTrigger>
       </div>
     );
   }
