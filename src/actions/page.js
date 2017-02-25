42c33f9b5a5b04083a5d03ba0490767838de5ae9
Josh Humpherys <joshua.humpherys@gmail.com>
2017-02-24 22:53:48 -0500

    Fix hashHistory warning

--- src/actions/page.js
+++ src/actions/page.js
@@ -2,6 +2,6 @@ import { hashHistory } from 'react-router'
 import * as actionTypes from '../constants/actionTypes'
 
 export function changePage(page) {
-  hashHistory.push(page);
+  hashHistory.replace(page);
   return { type: actionTypes.FAILED_REQUESTS_CLEARED }
 }
