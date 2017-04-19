859ee64b1cb7931b04406483c539811066a7778b
David Reed <dboyreed@charter.net>
2017-04-18 23:51:55 -0400

    ...

--- src/actions/grid.js
+++ src/actions/grid.js
@@ -17,3 +17,42 @@ export function loadGrid(grid) {
 export function updateGrid(grid) {
 	return { type: actionTypes.GRID_UPDATED, payload: { grid } };
 }
+
+function requestGrid(performanceId) {
+	return { type: actionTypes.GRID_REQUESTED, payload: { performanceId } };
+}
+
+function receiveGrid(orgId, performanceId, json) {
+	return { type: actionTypes.GRID_RECEIVED, payload: { performanceId, singerList: json } };
+}
+
+function fetchGrid(token, orgId, performanceId) {
+	return async dispatch => {
+		dispatch(requestGrid(performanceId));
+		let response = await fetch(config.baseApiUrl + '/organizations/' + orgId + '/performances/' + performanceId + '/grid/singers', {
+			method: 'GET',
+			headers: {
+				'Authorization': 'jwt ' + token,
+				'Content-Type': 'application/json'
+			}
+		});
+		let json = await response.json();
+		if (response.status == 200) {
+			dispatch(receiveGrid(orgId, performanceId, json));
+		} else {
+			// TODO: how to handle 404 errors?
+		}
+	};
+}
+
+function shouldFetchGrid(state) {
+	return state.grid.singersList == null && !state.isFetchingGrid;
+}
+
+export function fetchGridIfNeeded(token, orgId, performanceId) {
+	return (dispatch, getState) => {
+		if (shouldFetchGrid(getState())) {
+			return dispatch(fetchGrid(token, orgId, performanceId));
+		}
+	};
+}
